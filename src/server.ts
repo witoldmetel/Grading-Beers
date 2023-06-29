1a77c7426904df415b637e13f3a1ae191838615d
witoldmetel <witoldmetel@gmail.com>
2023-06-29 17:04:22 +0200

    add first hapi plugin

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import Hapi from "@hapi/hapi";
+import statusPlugin from "./plugins/status";
+
+const server: Hapi.Server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || "localhost",
+});
+
+export async function start(): Promise<Hapi.Server> {
+  await server.register([statusPlugin]);
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+  return server;
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+start().catch((err) => {
+  console.log(err);
+});
